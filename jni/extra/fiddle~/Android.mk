58be89e0a40d1a121961fd18b29864e85f8bb0da
Peter Brinkmann <peter.brinkmann@gmail.com>
2010-08-02 17:04:58 -0400

    fiddle~ still broken, but build system works now

--- jni/extra/fiddle~/Android.mk
+++ jni/extra/fiddle~/Android.mk
@@ -6,14 +6,11 @@ LOCAL_MODULE := fiddle~
 
 LOCAL_CFLAGS := -DPD
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/extra/fiddle~
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../src
 
-LOCAL_SRC_FILES := $(LOCAL_PATH)/extra/fiddle~/fiddle~.c
+LOCAL_SRC_FILES := fiddle~.c
 
 LOCAL_ALLOW_UNDEFINED_SYMBOLS := true
-
-# LOCAL_PRELINK_MODULE := false
-# LOCAL_LDLIBS := -lc
 
 include $(BUILD_SHARED_LIBRARY)
 
