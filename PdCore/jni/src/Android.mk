3c71d459e0000a81f86ddbf3a9159637d18a1717
Hans-Christoph Steiner <hans@at.or.at>
2010-08-11 13:04:44 -0400

    updated dummy api files in preparation for submission to Miller

--- PdCore/jni/src/Android.mk
+++ PdCore/jni/src/Android.mk
@@ -12,7 +12,7 @@ LOCAL_SRC_FILES := \
 	g_hslider.c g_io.c g_mycanvas.c g_numbox.c g_readwrite.c g_rtext.c \
 	g_scalar.c g_template.c g_text.c g_toggle.c g_traversal.c g_vdial.c \
 	g_vslider.c g_vumeter.c m_atom.c m_binbuf.c m_class.c m_conf.c m_glob.c \
-	m_memory.c m_obj.c m_pd.c m_sched.c s_audio.c s_audio_libpd.c s_entry.c \
+	m_memory.c m_obj.c m_pd.c m_sched.c s_audio.c s_audio_dummy.c s_entry.c \
 	s_file.c s_inter.c s_loader.c s_main.c s_midi.c s_midi_dummy.c s_path.c \
 	s_print.c x_acoustics.c x_arithmetic.c x_connective.c x_gui.c x_interface.c \
 	x_libpdreceive.c x_list.c x_midi.c x_misc.c x_net.c x_qlist.c x_time.c \
