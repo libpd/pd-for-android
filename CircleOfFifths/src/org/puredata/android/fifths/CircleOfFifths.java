40dc989f888744e99c1891b57a522c7126e6fc5f
Peter Brinkmann <peter.brinkmann@gmail.com>
2010-08-13 02:40:51 -0400

    initial commit of Java part of circle of fiths app

---
+++ CircleOfFifths/src/org/puredata/android/fifths/CircleOfFifths.java
@@ -0,0 +1,188 @@
+/**
+ * 
+ * @author Peter Brinkmann (peter.brinkmann@gmail.com)
+ * 
+ * For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ * WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+ * 
+ */
+
+package org.puredata.android.fifths;
+
+import org.puredata.android.service.IPdClient;
+import org.puredata.android.service.IPdService;
+import org.puredata.android.service.PdUtils;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class CircleOfFifths extends Activity {
+
+	private static final String PD_CIRCLE = "Pd Circle Of Fifths";
+	private final Handler handler = new Handler();
+	private IPdService pdServiceProxy = null;
+	private boolean hasAudio = false;
+	private TextView logs;
+	private String patch;
+
+	private void post(final String msg) {
+		handler.post(new Runnable() {
+			@Override
+			public void run() {
+				Toast.makeText(getApplicationContext(), PD_CIRCLE + ": " + msg, Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+	
+	private void log(final String msg) {
+		handler.post(new Runnable() {
+			@Override
+			public void run() {
+				logs.append(msg + ((msg.endsWith("\n")) ? "" : "\n"));
+			}
+		});
+	}
+
+	private final IPdClient.Stub statusWatcher = new IPdClient.Stub() {
+		@Override
+		public void requestUnbind() throws RemoteException {
+			post("Pure Data was stopped externally; exiting now");
+			finish();
+		}
+
+		@Override
+		public void audioChanged(int sampleRate, int nIn, int nOut, float bufferSizeMillis) throws RemoteException {
+			if (sampleRate > 0) {
+				post("Audio parameters: sample rate: " + sampleRate + ", input channels: " + nIn + ", output channels: " + nOut + 
+						", buffer size: " + bufferSizeMillis + "ms");
+			} else {
+				post("Audio stopped");
+			}
+		}
+
+		@Override
+		public void print(String s) throws RemoteException {
+			Log.i(PD_CIRCLE, s);
+		}
+	};
+
+	private final ServiceConnection serviceConnection = new ServiceConnection() {
+		@Override
+		public synchronized void onServiceDisconnected(ComponentName name) {
+			pdServiceProxy = null;
+			disconnected();
+		}
+
+		@Override
+		public synchronized void onServiceConnected(ComponentName name, IBinder service) {
+			pdServiceProxy = IPdService.Stub.asInterface(service);
+			initPd();
+		}
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		initGui();
+		bindService(new Intent(PdUtils.LAUNCH_ACTION), serviceConnection, BIND_AUTO_CREATE);
+	}
+
+	// this callback makes sure that we handle orientation changes without audio glitches
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		initGui();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		cleanup();
+	}
+
+	private void initGui() {
+		setContentView(R.layout.main);
+		CircleView circle = (CircleView) findViewById(R.id.circleview);
+		circle.setOwner(this);
+		logs = (TextView) findViewById(R.id.logview);
+		logs.setMovementMethod(new ScrollingMovementMethod());
+	}
+
+	private void initPd() {
+		Resources res = getResources();
+//		String path = res.getString(R.string.path_to_patch);
+		try {
+			pdServiceProxy.addClient(statusWatcher);
+//			patch = PdUtils.openPatch(pdServiceProxy, path);
+			int err = pdServiceProxy.requestAudio(-1, 1, 2, -1); // negative values default to PdService preferences
+			hasAudio = (err == 0);
+			if (!hasAudio) {
+				post("unable to start audio; exiting now");
+				finish();
+			}
+		} catch (RemoteException e) {
+			Log.e(PD_CIRCLE, e.toString());
+			disconnected();
+		} 
+//		catch (IOException e) {
+//			post(e.toString() + "; exiting now");
+//			finish();
+//		}
+	}
+
+	@Override
+	public void finish() {
+		cleanup();
+		super.finish();
+	}
+
+	private void disconnected() {
+		post("lost connection to Pd Service; exiting now");
+		finish();
+	}
+
+	private void cleanup() {
+		synchronized (serviceConnection) {  // on the remote chance that service gets disconnected while we're here
+			if (pdServiceProxy == null) return;
+			try {
+				// make sure to release all resources
+				pdServiceProxy.removeClient(statusWatcher);
+				PdUtils.closePatch(pdServiceProxy, patch);
+				if (hasAudio) pdServiceProxy.releaseAudio();  // only release audio if you actually have it...
+			} catch (RemoteException e) {
+				Log.e(PD_CIRCLE, e.toString());
+			}
+		}
+		try {
+			unbindService(serviceConnection);
+		} catch (IllegalArgumentException e) {
+			// already unbound
+			pdServiceProxy = null;
+		}
+	}
+
+	public void playChord(int n, boolean b) {
+		log("play: (" + n + ", " + (b ? "major" : "minor") + ")");
+	}
+
+	public void shift(int d) {
+		log("shift: " + d);
+	}
+
+	public void endChord() {
+		log("chord ended");
+	}
+}
---
+++ CircleOfFifths/src/org/puredata/android/fifths/CircleView.java
@@ -0,0 +1,150 @@
+/**
+ * 
+ * @author Peter Brinkmann (peter.brinkmann@gmail.com)
+ * 
+ * For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ * WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+ * 
+ */
+
+package org.puredata.android.fifths;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Paint.Style;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+public final class CircleView extends View {
+
+	private static final String TAG = "Pd Circle of Fifths";
+	private static final String[] notesSharp = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+	private static final String[] notesFlat  = { "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B" };
+	private static final int[] shifts =        {  0,   -5,   2,   -3,   4,   -1,  6,    1,   -4,   3,   -2,   5  };
+	private static final float R0 = 0.3f;
+	private static final float R1 = (float) Math.sqrt((1 + R0 * R0) / 2);  // equal area for major and minor fields
+	private final Paint black = new Paint();
+	private final Paint bgMajor = new Paint();
+	private final Paint bgMinor = new Paint();
+	private final Paint labels = new Paint();
+	private int top = 0;
+	private float xCenter, yCenter, xNorm, yNorm;
+	private int initialSegment;
+	private CircleOfFifths owner;
+
+	public CircleView(Context context) {
+		super(context);
+		init();
+	}
+
+	public CircleView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
+
+	public CircleView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init();
+	}
+
+	private void init() {
+		black.setColor(Color.BLACK);
+		bgMajor.setColor(Color.CYAN);
+		bgMinor.setColor(Color.GRAY);
+		labels.setColor(Color.BLACK);
+		labels.setStyle(Style.FILL_AND_STROKE);
+		labels.setTextAlign(Paint.Align.CENTER);
+		labels.setTextSize(0.2f);
+	}
+
+	public void setOwner(CircleOfFifths owner) {
+		this.owner = owner;
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		int xDim = getDim(widthMeasureSpec);
+		int yDim = getDim(heightMeasureSpec);
+		int dim = Math.min(xDim, yDim);
+		Log.i(TAG, "dimension: " + dim);
+		setMeasuredDimension(dim, dim);
+	}
+
+	private int getDim(int widthMeasureSpec) {
+		int mode = MeasureSpec.getMode(widthMeasureSpec);
+		int size = MeasureSpec.getSize(widthMeasureSpec);
+		return (mode == MeasureSpec.UNSPECIFIED) ? 320 : size;
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		int scale = getWidth() / 2;
+		canvas.scale(scale, scale);
+		canvas.translate(1, 1);
+		canvas.drawOval(new RectF(-1, -1, 1, 1), bgMajor);
+		canvas.drawOval(new RectF(-R1, -R1, R1, R1), bgMinor);
+		canvas.drawOval(new RectF(-R0, -R0, R0, R0), black);
+		int c = top;
+		int s0 = shifts[c];
+		for (int i = 0; i < 12; i++) {
+			int s1 = s0 + i;
+			if (i > 6) s1 -= 12;
+			String label = (s1 >= 0) ? notesSharp[c] : notesFlat[c];
+			canvas.drawText(label, 0, -(1 + R1) / 2.2f, labels);
+			c = (c + 9) % 12;
+			label = (s1 >= 0) ? notesSharp[c] : notesFlat[c];
+			canvas.drawText(label.toLowerCase(), 0, -(R1 + R0) / 2.2f, labels);
+			c = (c + 10) % 12;
+			canvas.rotate(15);
+			canvas.drawLine(0, -1, 0, 1, labels);
+			canvas.rotate(15);
+		}
+	}
+
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		xCenter = w / 2;
+		xNorm = 1 / xCenter;
+		yCenter = h / 2;
+		yNorm = 1 / yCenter;
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		float x = (event.getX() - xCenter) * xNorm;
+		float y = (event.getY() - yCenter) * yNorm;
+		float angle = (float) (Math.atan2(x, -y) * 12 / Math.PI);
+		int segment = (int) (angle + 24.5f) % 24;
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			initialSegment = segment;
+			if (segment % 2 == 0) {
+				float radius = x * x + y * y;
+				boolean major = radius > R1 * R1;
+				int note = (top + segment * 7 / 2 + (major ? 0 : 9)) % 12;
+				owner.playChord(note, major);
+			}
+			break;
+		case MotionEvent.ACTION_MOVE:
+			int step = (initialSegment - segment + 24) % 24;
+			if (step > 1 && step < 23) {
+				initialSegment = segment;
+				int shift = (step / 2) % 12;
+				top = (top + shift * 7 + 24 * 12) % 12;
+				invalidate();
+				owner.shift(shift);
+			}
+			break;
+		case MotionEvent.ACTION_UP:
+		default:
+			owner.endChord();
+			break;
+		}
+		return true;
+	}
+}
