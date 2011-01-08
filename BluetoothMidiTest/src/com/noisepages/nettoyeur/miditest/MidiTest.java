73ee77943774153d0adc4533a0d3bcf3b3340599
Peter Brinkmann <peter.brinkmann@gmail.com>
2011-01-07 20:41:40 -0500

    license adjustment

--- BluetoothMidiTest/src/com/noisepages/nettoyeur/miditest/MidiTest.java
+++ BluetoothMidiTest/src/com/noisepages/nettoyeur/miditest/MidiTest.java
@@ -1,17 +1,10 @@
-/*
- * Copyright (C) 2011 Peter Brinkmann (peter.brinkmann@gmail.com)
+/**
+ * 
+ * For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ * WARRANTIES, see the file, "LICENSE.txt," in this distribution.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * simple test case for {@link PdService}
+ * 
  */
 
 package com.noisepages.nettoyeur.miditest;
