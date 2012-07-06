1faf42a56ea01668fc3e3a88e046d60600683ddd
Peter Brinkmann <peter.brinkmann@gmail.com>
2012-07-05 20:14:30 -0400

    added support for mips and x86 and fixed RjDj externals

--- PdCore/src/org/puredata/android/utils/Properties.java
+++ PdCore/src/org/puredata/android/utils/Properties.java
@@ -23,15 +23,4 @@ public class Properties {
 	 */
 	public static final int version = Integer.parseInt(Build.VERSION.SDK);
 	
-	/**
-	 * true if and only if armeabi-v7a is available
-	 */
-	public static final boolean hasArmeabiV7a = version >= 4 && VersionedAbiCheck.hasV7a();
-	
-	// use lazy class loading to hide Build.CPU_ABI from Cupcake
-	private static class VersionedAbiCheck {
-		private static boolean hasV7a() {
-			return "armeabi-v7a".equals(Build.CPU_ABI);
-		}
-	}
 }
--- ScenePlayer/jni/Android.mk
+++ ScenePlayer/jni/Android.mk
@@ -4,50 +4,50 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := rj_accum
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../pd-for-android/PdCore/jni/src
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../PdCore/jni/libpd/pure-data/src
 LOCAL_CFLAGS := -DPD
 LOCAL_SRC_FILES := rj_accum.c
-LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../../pd-for-android/PdCore/libs/armeabi -lpdnative
+LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../PdCore/libs/$(TARGET_ARCH_ABI) -lpd
 include $(BUILD_SHARED_LIBRARY)
 
 #---------------------------------------------------------------
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := rj_senergy~
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../pd-for-android/PdCore/jni/src
+LOCAL_MODULE := rj_senergy_tilde
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../PdCore/jni/libpd/pure-data/src
 LOCAL_CFLAGS := -DPD
 LOCAL_SRC_FILES := rj_senergy~.c
-LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../../pd-for-android/PdCore/libs/armeabi -lpdnative
+LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../PdCore/libs/$(TARGET_ARCH_ABI) -lpd
 include $(BUILD_SHARED_LIBRARY)
 
 #---------------------------------------------------------------
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := rj_barkflux_accum~
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../pd-for-android/PdCore/jni/src
+LOCAL_MODULE := rj_barkflux_accum_tilde
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../PdCore/jni/libpd/pure-data/src
 LOCAL_CFLAGS := -DPD
 LOCAL_SRC_FILES := rj_barkflux_accum~.c
-LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../../pd-for-android/PdCore/libs/armeabi -lpdnative
+LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../PdCore/libs/$(TARGET_ARCH_ABI) -lpd
 include $(BUILD_SHARED_LIBRARY)
 
 #---------------------------------------------------------------
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := rj_centroid~
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../pd-for-android/PdCore/jni/src
+LOCAL_MODULE := rj_centroid_tilde
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../PdCore/jni/libpd/pure-data/src
 LOCAL_CFLAGS := -DPD
 LOCAL_SRC_FILES := rj_centroid~.c
-LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../../pd-for-android/PdCore/libs/armeabi -lpdnative
+LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../PdCore/libs/$(TARGET_ARCH_ABI) -lpd
 include $(BUILD_SHARED_LIBRARY)
 
 #---------------------------------------------------------------
 
-LOCAL_MODULE := rj_zcr~
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../pd-for-android/PdCore/jni/src
+include $(CLEAR_VARS)
+LOCAL_MODULE := rj_zcr_tilde
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../PdCore/jni/libpd/pure-data/src
 LOCAL_CFLAGS := -DPD
 LOCAL_SRC_FILES := rj_zcr~.c
-LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../../pd-for-android/PdCore/libs/armeabi -lpdnative
+LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../PdCore/libs/$(TARGET_ARCH_ABI) -lpd
 include $(BUILD_SHARED_LIBRARY)
 
 #---------------------------------------------------------------
-
