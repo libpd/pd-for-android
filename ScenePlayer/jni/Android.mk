647eb9062c97ae5ee52bba1b7c212092c656dce7
Joe Bowbeer <joe.bowbeer@gmail.com>
2016-02-07 22:10:27 -0800

    Fix Issue #33: non-system libraries in linker flags

--- ScenePlayer/jni/Android.mk
+++ ScenePlayer/jni/Android.mk
@@ -3,51 +3,56 @@ LOCAL_PATH := $(call my-dir)
 #---------------------------------------------------------------
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := pd
+LOCAL_EXPORT_C_INCLUDES := ../../PdCore/jni/libpd/pure-data/src
+LOCAL_SRC_FILES := ../../PdCore/libs/$(TARGET_ARCH_ABI)/libpd.so
+ifneq ($(MAKECMDGOALS),clean)
+    include $(PREBUILT_SHARED_LIBRARY)
+endif
+
+#---------------------------------------------------------------
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := rj_accum
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../PdCore/jni/libpd/pure-data/src
 LOCAL_CFLAGS := -DPD
 LOCAL_SRC_FILES := rj_accum.c
-LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../PdCore/libs/$(TARGET_ARCH_ABI) -lpd
+LOCAL_SHARED_LIBRARIES = pd
 include $(BUILD_SHARED_LIBRARY)
 
 #---------------------------------------------------------------
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := rj_senergy_tilde
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../PdCore/jni/libpd/pure-data/src
 LOCAL_CFLAGS := -DPD
 LOCAL_SRC_FILES := rj_senergy~.c
-LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../PdCore/libs/$(TARGET_ARCH_ABI) -lpd
+LOCAL_SHARED_LIBRARIES = pd
 include $(BUILD_SHARED_LIBRARY)
 
 #---------------------------------------------------------------
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := rj_barkflux_accum_tilde
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../PdCore/jni/libpd/pure-data/src
 LOCAL_CFLAGS := -DPD
 LOCAL_SRC_FILES := rj_barkflux_accum~.c
-LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../PdCore/libs/$(TARGET_ARCH_ABI) -lpd
+LOCAL_SHARED_LIBRARIES = pd
 include $(BUILD_SHARED_LIBRARY)
 
 #---------------------------------------------------------------
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := rj_centroid_tilde
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../PdCore/jni/libpd/pure-data/src
 LOCAL_CFLAGS := -DPD
 LOCAL_SRC_FILES := rj_centroid~.c
-LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../PdCore/libs/$(TARGET_ARCH_ABI) -lpd
+LOCAL_SHARED_LIBRARIES = pd
 include $(BUILD_SHARED_LIBRARY)
 
 #---------------------------------------------------------------
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := rj_zcr_tilde
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../PdCore/jni/libpd/pure-data/src
 LOCAL_CFLAGS := -DPD
 LOCAL_SRC_FILES := rj_zcr~.c
-LOCAL_LDLIBS := -L$(LOCAL_PATH)/../../PdCore/libs/$(TARGET_ARCH_ABI) -lpd
+LOCAL_SHARED_LIBRARIES = pd
 include $(BUILD_SHARED_LIBRARY)
 
 #---------------------------------------------------------------
