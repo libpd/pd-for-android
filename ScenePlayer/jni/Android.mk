0cf0612f421c7f7b813da2073ba0ec58238ceb98
jamesnitsch <James.Nitsch@WillowtreeApps.com>
2019-11-26 11:41:58 -0500

    Update ScenePlayer and PdTest project src file locations

--- ScenePlayer/jni/Android.mk
+++ ScenePlayer/jni/Android.mk
@@ -4,8 +4,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := pd
-LOCAL_EXPORT_C_INCLUDES := ../../PdCore/jni/libpd/pure-data/src
-LOCAL_SRC_FILES := ../../PdCore/libs/$(TARGET_ARCH_ABI)/libpd.so
+LOCAL_EXPORT_C_INCLUDES := ../../PdCore/src/main/jni/libpd/pure-data/src
+LOCAL_SRC_FILES := ../../PdCore/src/main/libs/$(TARGET_ARCH_ABI)/libpd.so
 ifneq ($(MAKECMDGOALS),clean)
     include $(PREBUILT_SHARED_LIBRARY)
 endif
