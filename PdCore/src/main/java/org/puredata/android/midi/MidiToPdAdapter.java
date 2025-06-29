7296d0c47b8f340bcd209c66bde74825b4f6935b
Antoine Rousseau <_antoine_@metalu.net>
2025-06-29 15:12:04 +0200

    Give up com.noisepages.nettoyeur:midi
    Reimplement Midi adapters using Android Midi API,
    reusing Peter's (from/to)WireConverters coming from:
    https://github.com/nettoyeurny/btmidi/blob/master/AndroidMidi/src/com/noisepages/nettoyeur/midi/

--- PdCore/src/main/java/org/puredata/android/midi/MidiToPdAdapter.java
+++ PdCore/src/main/java/org/puredata/android/midi/MidiToPdAdapter.java
@@ -8,61 +8,88 @@
 package org.puredata.android.midi;
 
 import org.puredata.core.PdBase;
-
-import com.noisepages.nettoyeur.midi.MidiReceiver;
+import android.media.midi.MidiReceiver;
 
 /**
  * Adapter class for connecting output from AndroidMidi to MIDI input for Pd.
  * 
  * @author Peter Brinkmann (peter.brinkmann@gmail.com)
+ * @author Antoine Rousseau (antoine@metalu.net)
  */
-public class MidiToPdAdapter implements MidiReceiver {
+public class MidiToPdAdapter extends MidiReceiver {
+	private static enum State {
+		NOTE_OFF, NOTE_ON, POLY_TOUCH, CONTROL_CHANGE, PROGRAM_CHANGE, AFTERTOUCH, PITCH_BEND, NONE
+	}
+	private State midiState = State.NONE;
+	private int channel;
+	private int firstByte;
 
 	@Override
-	public void onRawByte(byte value) {
-		PdBase.sendMidiByte(0, value);
+	public void onSend(byte[] msg, int offset, int count, long timestamp) {
+		while(count-- != 0) processByte(msg[offset++]);
 	}
 
-	@Override
-	public void onProgramChange(int channel, int program) {
-		PdBase.sendProgramChange(channel, program);
+	private void processByte(int b) {
+		if (b < 0) {
+			midiState = State.values()[(b >> 4) & 0x07];
+			if (midiState != State.NONE) {
+				channel = b & 0x0f;
+				firstByte = -1;
+			} else {
+				PdBase.sendMidiByte(0, b);
+			}
+		} else {
+			switch (midiState) {
+			case NOTE_OFF:
+				if (firstByte < 0) {
+					firstByte = b;
+				} else {
+					PdBase.sendNoteOn(channel, firstByte, 0);
+					firstByte = -1;
+				}
+				break;
+			case NOTE_ON:
+				if (firstByte < 0) {
+					firstByte = b;
+				} else {
+					PdBase.sendNoteOn(channel, firstByte, b);
+					firstByte = -1;
+				}
+				break;
+			case POLY_TOUCH:
+				if (firstByte < 0) {
+					firstByte = b;
+				} else {
+					PdBase.sendPolyAftertouch(channel, firstByte, b);
+					firstByte = -1;
+				}
+				break;
+			case CONTROL_CHANGE:
+				if (firstByte < 0) {
+					firstByte = b;
+				} else {
+					PdBase.sendControlChange(channel, firstByte, b);
+					firstByte = -1;
+				}
+				break;
+			case PROGRAM_CHANGE:
+				PdBase.sendProgramChange(channel, b);
+				break;
+			case AFTERTOUCH:
+				PdBase.sendAftertouch(channel, b);
+				break;
+			case PITCH_BEND:
+				if (firstByte < 0) {
+					firstByte = b;
+				} else {
+					PdBase.sendPitchBend(channel, ((b << 7) | firstByte) - 8192);
+					firstByte = -1;
+				}
+				break;
+				default /* State.NONE */:
+				PdBase.sendMidiByte(0, b);
+				break;
+			}
+		}
 	}
-
-	@Override
-	public void onPolyAftertouch(int channel, int key, int velocity) {
-		PdBase.sendPolyAftertouch(channel, key, velocity);
-	}
-
-	@Override
-	public void onPitchBend(int channel, int value) {
-		PdBase.sendPitchBend(channel, value);
-	}
-
-	@Override
-	public void onNoteOn(int channel, int key, int velocity) {
-		PdBase.sendNoteOn(channel, key, velocity);
-	}
-
-	@Override
-	public void onNoteOff(int channel, int key, int velocity) {
-		PdBase.sendNoteOn(channel, key, 0);
-	}
-
-	@Override
-	public void onControlChange(int channel, int controller, int value) {
-		PdBase.sendControlChange(channel, controller, value);
-	}
-
-	@Override
-	public void onAftertouch(int channel, int velocity) {
-		PdBase.sendAftertouch(channel, velocity);
-	}
-
-	@Override
-	public boolean beginBlock() {
-		return false;
-	}
-
-	@Override
-	public void endBlock() {}
 }
