62a87399080244ca5f4b3711a037142c55030ff1
Antoine Rousseau <_antoine_@metalu.net>
2025-06-29 15:12:04 +0200

    Give up com.noisepages.nettoyeur:midi
    Reimplement Midi adapters using Android Midi API,
    reusing Peter's (from/to)WireConverters coming from:
    https://github.com/nettoyeurny/btmidi/blob/master/AndroidMidi/src/com/noisepages/nettoyeur/midi/

--- PdCore/src/main/java/org/puredata/android/midi/PdToMidiAdapter.java
+++ PdCore/src/main/java/org/puredata/android/midi/PdToMidiAdapter.java
@@ -8,70 +8,84 @@
 package org.puredata.android.midi;
 
 import org.puredata.core.PdMidiReceiver;
-
-import com.noisepages.nettoyeur.midi.MidiReceiver;
+import android.media.midi.MidiInputPort;
 
 /**
  * Adapter class for connecting MIDI output from Pd to input for AndroidMidi.
  * 
  * @author Peter Brinkmann (peter.brinkmann@gmail.com)
+ * @author Antoine Rousseau (antoine@metalu.net)
  */
 public class PdToMidiAdapter implements PdMidiReceiver {
+	private final MidiInputPort inputPort;
 
-	private final MidiReceiver receiver;
-	
-	/**
-	 * Constructor. Note that instances of this class still need to be installed with
-	 * PdBase.setMidiReceiver.
-	 * 
-	 * @param receiver to forward MIDI messages to
-	 */
-	public PdToMidiAdapter(MidiReceiver receiver) {
-		this.receiver = receiver;
+	public PdToMidiAdapter(MidiInputPort inputPort) {
+		this.inputPort = inputPort;
 	}
-	
-	@Override
-	public void receiveProgramChange(int channel, int value) {
-		receiver.onProgramChange(channel, value);
-	}
-	
-	@Override
-	public void receivePolyAftertouch(int channel, int pitch, int value) {
-		receiver.onPolyAftertouch(channel, pitch, value);
-	}
-	
-	@Override
-	public void receivePitchBend(int channel, int value) {
-		receiver.onPitchBend(channel, value);
-	}
-	
+
 	@Override
 	public void receiveNoteOn(int channel, int pitch, int velocity) {
-		receiver.onNoteOn(channel, pitch, velocity);
+		write(0x90, channel, pitch, velocity);
 	}
-	
+
 	@Override
-	public void receiveMidiByte(int port, int value) {
-		receiver.onRawByte((byte) value);
+	public void receivePolyAftertouch(int channel, int pitch, int value) {
+		write(0xa0, channel, pitch, value);
 	}
-	
+
 	@Override
 	public void receiveControlChange(int channel, int controller, int value) {
-		receiver.onControlChange(channel, controller, value);
+		write(0xb0, channel, controller, value);
 	}
-	
+
+	@Override
+	public void receiveProgramChange(int channel, int program) {
+		write(0xc0, channel, program);
+	}
+
 	@Override
 	public void receiveAftertouch(int channel, int value) {
-		receiver.onAftertouch(channel, value);
+		write(0xd0, channel, value);
+	}
+
+	@Override
+	public void receivePitchBend(int channel, int value) {
+		value += 8192;
+		write(0xe0, channel, (value & 0x7f), (value >> 7));
+	}
+
+	@Override
+	public void receiveMidiByte(int port, int value) {
+		final byte[] message = {(byte) value};
+		writeMessage(message);
+	}
+
+	private static byte firstByte(int msg, int ch) {
+		return (byte) (msg | (ch & 0x0f));
+	}
+
+	private void write(int msg, int ch, int a) {
+		final byte[] message = {firstByte(msg, ch), (byte) a};
+		writeMessage(message);
+	}
+
+	private void write(int msg, int ch, int a, int b) {
+		final byte[] message = {firstByte(msg, ch), (byte) a, (byte) b};
+		writeMessage(message);
+	}
+
+	private void writeMessage(byte[] message) {
+		try {
+			inputPort.send(message, 0, message.length);
+		} catch(Exception e) {}
 	}
 
 	@Override
 	public boolean beginBlock() {
-		return receiver.beginBlock();
+		return false;
 	}
 
 	@Override
 	public void endBlock() {
-		receiver.endBlock();
 	}
 }
