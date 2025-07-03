49ff174f121922d4fb1abd38ca4b5c86fe65e046
Antoine Rousseau <_antoine_@metalu.net>
2025-07-03 17:36:42 +0200

    fix compilation and execution of examples with externals

--- PdTest/jni/Android.mk
+++ PdTest/jni/Android.mk
@@ -5,7 +5,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := pd
 LOCAL_EXPORT_C_INCLUDES := ../../PdCore/src/main/jni/libpd/pure-data/src
-LOCAL_SRC_FILES := ../../PdCore/src/main/libs/$(TARGET_ARCH_ABI)/libpd.so
+LOCAL_SRC_FILES := ../../PdCore/build/intermediates/library_jni/$(APP_OPTIM)/jni/$(TARGET_ARCH_ABI)/libpd.so
+#../../PdCore/src/main/libs/$(TARGET_ARCH_ABI)/libpd.so
 ifneq ($(MAKECMDGOALS),clean)
     include $(PREBUILT_SHARED_LIBRARY)
 endif
