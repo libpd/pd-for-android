ed40f4f0b438d765ccfa713657915416446786c3
Antoine Rousseau <_antoine_@metalu.net>
2025-07-04 17:02:50 +0200

    PdTest and ScenePlayer: update native build and use PdCore prefab

--- PdTest/jni/Android.mk
+++ PdTest/jni/Android.mk
@@ -2,13 +2,13 @@ LOCAL_PATH := $(call my-dir)
 
 #---------------------------------------------------------------
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := pd
-LOCAL_EXPORT_C_INCLUDES := ${LOCAL_PATH}/../../PdCore/src/main/jni/libpd/pure-data/src
-LOCAL_SRC_FILES := ${LOCAL_PATH}/../../PdCore/build/intermediates/library_jni/$(APP_OPTIM)/jni/$(TARGET_ARCH_ABI)/libpd.so
-ifneq ($(MAKECMDGOALS),clean)
-    include $(PREBUILT_SHARED_LIBRARY)
-endif
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := pd
+#LOCAL_EXPORT_C_INCLUDES := ${LOCAL_PATH}/../../PdCore/src/main/jni/libpd/pure-data/src
+#LOCAL_SRC_FILES := ${LOCAL_PATH}/../../PdCore/build/intermediates/library_jni/$(APP_OPTIM)/jni/$(TARGET_ARCH_ABI)/libpd.so
+#ifneq ($(MAKECMDGOALS),clean)
+#    include $(PREBUILT_SHARED_LIBRARY)
+#endif
 
 #---------------------------------------------------------------
 
@@ -18,5 +18,12 @@ LOCAL_CFLAGS := -DPD
 LOCAL_SRC_FILES := helloworld.c
 LOCAL_SHARED_LIBRARIES = pd
 include $(BUILD_SHARED_LIBRARY)
+
+# If you don't need your project to build with NDKs older than r21, you can omit
+# this block.
+ifneq ($(call ndk-major-at-least,21),true)
+    $(call import-add-path,$(NDK_GRADLE_INJECTED_IMPORT_PATH))
+endif
+$(call import-module,prefab/PdCore)
 
 #---------------------------------------------------------------
--- ScenePlayer/jni/Android.mk
+++ ScenePlayer/jni/Android.mk
@@ -2,13 +2,13 @@ LOCAL_PATH := $(call my-dir)
 
 #---------------------------------------------------------------
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := pd
-LOCAL_EXPORT_C_INCLUDES := ../../PdCore/src/main/jni/libpd/pure-data/src
-LOCAL_SRC_FILES := ../../PdCore/build/intermediates/library_jni/$(APP_OPTIM)/jni/$(TARGET_ARCH_ABI)/libpd.so
-ifneq ($(MAKECMDGOALS),clean)
-    include $(PREBUILT_SHARED_LIBRARY)
-endif
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := pd
+#LOCAL_EXPORT_C_INCLUDES := ../../PdCore/src/main/jni/libpd/pure-data/src
+#LOCAL_SRC_FILES := ../../PdCore/build/intermediates/library_jni/$(APP_OPTIM)/jni/$(TARGET_ARCH_ABI)/libpd.so
+#ifneq ($(MAKECMDGOALS),clean)
+#    include $(PREBUILT_SHARED_LIBRARY)
+#endif
 
 #---------------------------------------------------------------
 
@@ -56,3 +56,11 @@ LOCAL_SHARED_LIBRARIES = pd
 include $(BUILD_SHARED_LIBRARY)
 
 #---------------------------------------------------------------
+
+# If you don't need your project to build with NDKs older than r21, you can omit
+# this block.
+ifneq ($(call ndk-major-at-least,21),true)
+    $(call import-add-path,$(NDK_GRADLE_INJECTED_IMPORT_PATH))
+endif
+$(call import-module,prefab/PdCore)
+
