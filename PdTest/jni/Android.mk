97e4a322823fcc1e643ab7c31615b40085a04fd1
Antoine Rousseau <_antoine_@metalu.net>
2025-07-13 22:38:41 +0200

    clean up

--- PdTest/jni/Android.mk
+++ PdTest/jni/Android.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
+#---------------------------------------------------------------
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := helloworld
 LOCAL_CFLAGS := -DPD
@@ -7,11 +9,7 @@ LOCAL_SRC_FILES := helloworld.c
 LOCAL_SHARED_LIBRARIES = pd
 include $(BUILD_SHARED_LIBRARY)
 
-# If you don't need your project to build with NDKs older than r21, you can omit
-# this block.
-ifneq ($(call ndk-major-at-least,21),true)
-    $(call import-add-path,$(NDK_GRADLE_INJECTED_IMPORT_PATH))
-endif
+#---------------------------------------------------------------
+
 $(call import-module,prefab/pd-core)
 
-#---------------------------------------------------------------
